6c5be451cbb0ed8bcf2a097d7013cd36dcd192f7
diarcaz <125239227+diarcaz@users.noreply.github.com>
2024-06-13 00:45:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ import { ref, set, push } from 'firebase/database';
 const products = [
     { name: 'Helado de Vainilla', price: 5.00, image: 'ruta_a_imagen_vainilla' },
     { name: 'Helado de Chocolate', price: 6.00, image: 'ruta_a_imagen_chocolate' },
-    { name: 'Sorbet de Mango', price: 4.50, image: 'ruta_a_imagen_mango' },
     { name: 'Helado de Fresa', price: 5.50, image: 'ruta_a_imagen_fresa' },
-    { name: 'Helado de Pistacho', price: 6.50, image: 'ruta_a_imagen_pistacho' }
+    { name: 'Helado de Pistacho', price: 6.50, image: 'ruta_a_imagen_pistacho' },
+    { name: 'Sorbet de Mango', price: 4.50, image: 'ruta_a_imagen_mango' },
+    { name: 'Sorbet de Limón', price: 4.00, image: 'ruta_a_imagen_limon' },
+    { name: 'Sorbet de Frambuesa', price: 4.75, image: 'ruta_a_imagen_frambuesa' },
+    { name: 'Helado de Menta con Chispas de Chocolate', price: 5.75, image: 'ruta_a_imagen_menta' },
+    { name: 'Helado de Cookies and Cream', price: 6.00, image: 'ruta_a_imagen_cookies' },
+    { name: 'Helado de Café', price: 5.50, image: 'ruta_a_imagen_cafe' }
 ];
-
-const cart = [];
 
 function renderProducts() {
     const productGrid = document.querySelector('.product-grid');
